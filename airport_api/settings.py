120fc5ebb9a9c9007331f63241912feff0b6fb7c
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-16 20:53:50 +0200

    Register new apps in settings and install rest_framework

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "core",
+    "user",
 ]
 
 AUTH_USER_MODEL = "user.User"

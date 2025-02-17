6fec286adb15bce1e08896820302c32674ecbeda
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-17 12:42:28 +0200

    Models: Create Airport model

--- core/models.py
+++ core/models.py
@@ -10,3 +10,17 @@ class City(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    iata_code = models.CharField(max_length=3, unique=True)
+    city = models.ForeignKey(
+        City, on_delete=models.CASCADE, related_name="airports"
+    )
+
+    class Meta:
+        ordering = ("city__country", "city__name", "name")
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.iata_code}"

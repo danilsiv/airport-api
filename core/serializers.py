a021a511e11fb5bf459de37cf458dc17d5deb282
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-19 15:27:11 +0200

    Add serializers for Airport model

--- core/serializers.py
+++ core/serializers.py
@@ -1,9 +1,26 @@
 from rest_framework import serializers
 
-from core.models import City
+from core.models import City, Airport
 
 
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
         fields = ("id", "name", "country")
+
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = ("id", "name", "iata_code", "city")
+
+
+class AirportListSerializer(AirportSerializer):
+    city = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field="name"
+    )
+
+
+class AirportRetrieveSerializer(AirportSerializer):
+    city = CitySerializer()

447a5466c98d1cffb70986efe41d3c572e9f3515
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-28 12:37:10 +0200

    Create LoginUserView with URL mapping

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateUserView.as_view(), name="user-register"),
+    path("login/", LoginUserView.as_view(), name="user-login"),
 ]
 
 app_name = "user"

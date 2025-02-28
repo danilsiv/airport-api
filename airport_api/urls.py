4864f336956e5dc77986f426d7045670fda21eeb
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-28 11:05:51 +0200

    Add urls.py for user app and include in global urls

--- airport_api/urls.py
+++ airport_api/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/core/", include("core.urls", namespace="core")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]

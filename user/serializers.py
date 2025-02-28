d6a5d75275ea630f7924487be9812e151a28a7d0
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-02-28 13:16:29 +0200

    Update UserSerializer and add ManageUserView with URL mapping

--- user/serializers.py
+++ user/serializers.py
@@ -12,5 +12,16 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data) -> type:
-        """Create User with encrypted password """
+        """Create User with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update User with encrypted password."""
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from user.views import CreateUserView, LoginUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="user-register"),
-    path("login/", LoginUserView.as_view(), name="user-login"),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("login/", LoginUserView.as_view(), name="login"),
+    path("me/", ManageUserView.as_view(), name="profile"),
 ]
 
 app_name = "user"

c1a9f1b72075552a636803aad788a30dbd5585f8
Sivkovich Danylo <danilsivkovic@gmail.com>
2025-03-09 17:33:02 +0200

    Add tests for user views

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from user.serializers import UserSerializer
+
+
+class UserApiTest(TestCase):
+    def setUp(self) -> None:
+        self.client = APIClient()
+
+
+    def test_create_and_manage_user(self) -> None:
+        payload = {
+            "email": "test@user.com",
+            "password": "test123user"
+        }
+        response = self.client.post(reverse("user:register"), payload)
+        user = get_user_model().objects.get(email=response.data["email"])
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(payload["email"], user.email)
+        self.assertTrue(user.check_password(payload["password"]))
+
+        self.client.force_authenticate(user)
+        response = self.client.get(reverse("user:profile"))
+        serializer = UserSerializer(user)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+        updated_data = {
+            "email": "updated@user.com",
+            "password": "updated123user"
+        }
+
+        response = self.client.patch(reverse("user:profile"), updated_data)
+        user.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(user.email, updated_data["email"])
+        self.assertTrue(user.check_password(updated_data["password"]))
